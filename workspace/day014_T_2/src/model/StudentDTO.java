1bbe5ca83d2afeb403aeada878120ac11d452266
Gitkonium <leesstudymail0204@gmail.com>
2023-12-12 20:39:50 +0900

    [update] 14일차 복습 프로젝트 버전2

---
+++ workspace/day014_T_2/src/model/StudentDTO.java
@@ -0,0 +1,34 @@
+package model;
+
+public class StudentDTO {
+	private int num; // PK
+	private String name;
+	private int score;
+	public StudentDTO(int num,String name,int score) {
+		this.num=num;
+		this.name=name;
+		this.score=score;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	@Override
+	public String toString() {
+		return "Student [num=" + num + ", name=" + name + ", score=" + score + "]";
+	}
+}
